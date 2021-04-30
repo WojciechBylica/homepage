4f03832b675458506190e185a80ae32ee6e84632
Wojciech Bylica <wojtekbyl@gmail.com>
2021-04-30 15:08:09 +0200

    init funcion added

--- js/script.js
+++ js/script.js
@@ -4,34 +4,38 @@
     };
 
     const artsDegree = () => {
-        const mgr = document.querySelector(".js-mgr");
-        mgr.addEventListener("click", () => {
-            const title = document.querySelector(".js-title");
-            title.innerText = title.innerText === "Wojciech Bylica" ? "mgr Wojciech Bylica" : "Wojciech Bylica";
-            mgr.innerText = mgr.innerText === "wyświetl tytuł naukowy" ? "ukryj tytuł naukowy" : "wyświetl tytuł naukowy";
-        });
-    }
+        const title = document.querySelector(".js-title");
+        title.innerText = title.innerText === "Wojciech Bylica" ? "mgr Wojciech Bylica" : "Wojciech Bylica";
+        mgr.innerText = mgr.innerText === "wyświetl tytuł naukowy" ? "ukryj tytuł naukowy" : "wyświetl tytuł naukowy";
+    };
+
 
     const backGround = () => {
-        const button = document.querySelector(".js-button");
-        button.addEventListener("click", () => {
-            const gallery = document.querySelector(".js-gallery");
-            const galleryButtonText = document.querySelector(".js-galleryButtonText");
-            gallery.classList.toggle("section__gallery--hidden");
-            galleryButtonText.innerText = gallery.classList.contains("section__gallery--hidden") ? "wyświetlić" : "ukryć";
-        });
-    }
+        const galleryButtonText = document.querySelector(".js-galleryButtonText");
+        const gallery = document.querySelector(".js-gallery");
+        gallery.classList.toggle("section__gallery--hidden");
+        galleryButtonText.innerText = gallery.classList.contains("section__gallery--hidden") ? "wyświetlić" : "ukryć";
+    };
+
 
     const gallery = () => {
-        const button3 = document.querySelector(".js-button3");
-        button3.addEventListener("click", () => {
-            const tlo1 = document.querySelector(".header");
-            tlo1.classList.toggle("header--change")
-        });
+        const tlo1 = document.querySelector(".header");
+        tlo1.classList.toggle("header--change");
     }
 
-    welcome();
-    artsDegree();
-    backGround();
-    gallery();
+    init = () => {
+        welcome();
+
+        const button3 = document.querySelector(".js-button3");
+        button3.addEventListener("click", gallery);
+
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", backGround);
+
+        const mgr = document.querySelector(".js-mgr");
+        mgr.addEventListener("click", artsDegree);
+    }
+
+    init();
+
 }
